0a85cf01762ab8f612c663d8f487660f46e93a15
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2018-08-21 05:31:22 -0700

    parametrize num_consumers

--- cpp-futures/prodcon.cpp
+++ cpp-futures/prodcon.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+#define NUM_CONSUMERS 10
+
 bool consumer(int N)
 {
     printf("0x%lx:%d\n", (long)pthread_self(), N);
@@ -11,8 +13,8 @@ bool consumer(int N)
 void producer()
 {
     int i = 0;
-    std::future<bool> fu[10];
-    for (; i < 10; i++)
+    std::future<bool> fu[NUM_CONSUMERS];
+    for (; i < NUM_CONSUMERS; i++)
     {
         fu[i] = std::async(
                     std::launch::async | std::launch::deferred,
@@ -20,7 +22,7 @@ void producer()
                     i);
     }
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < NUM_CONSUMERS; i++)
     {
         bool done = fu[i].get();
 		printf(

fe2bbd843755f2029fc5b3855fc6630ebd70bbf3
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2018-08-31 09:54:33 +0100

    adding more comments to the erlang program

--- erlang-actors/prodcon.erl
+++ erlang-actors/prodcon.erl
@@ -1,42 +1,38 @@
 -module(prodcon).
 -export([start/0, consumer/1, producer/2]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-producer(_, 0) ->
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+producer(_, 0) ->                      % termination condition for recursion
     io:format("~n~nproducer finished~n"),
     true;
 producer(TheConsumer, N) ->
-    TheConsumer ! {N},
-    producer(TheConsumer, N-1).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    TheConsumer ! {N},                 % send N to the consumer
+    producer(TheConsumer, N-1).        % tail recursion with N-1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 consumer_fn(Parent, N) ->
-	io:format("~w~n", [N]),
-        if
-            1 == N -> Parent ! {N};
-            true -> consumer(Parent)
-        end.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-consumer(Parent) ->
-    receive
-        {N} -> consumer_fn(Parent, N);
-        stop -> true
+    io:format("~w~n", [N]),
+    if
+        1 == N -> Parent ! {N};        % if N = 1, send completion msg to start 
+        true -> consumer(Parent)       % else consume again
     end.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+consumer(Parent) ->
+    receive                            % receive a message from producer
+        {N} -> consumer_fn(Parent, N); % call consumer function if it is a num
+        stop -> true                   % stop if it is a stop message
+    end.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 start() ->
     Consumer = spawn(prodcon, consumer, [self()]),
     Producer = spawn(prodcon, producer, [Consumer, 10]),
     io:format("Producer = ~p Consumer = ~p", [Producer, Consumer]),
     io:format("~n~nstart finished~n"),
-    receive {N} ->
+    receive {N} ->                     % wait for consumer's completion msg
 	io:format("exiting~n"),
-        Consumer ! stop
+        Consumer ! stop                % ask the consumer to stop
     end.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

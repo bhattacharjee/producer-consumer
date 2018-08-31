a9ee303aa3d656e1a3236b713e86b3c89992e720
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2018-08-31 09:58:04 +0100

    adding some comments to the go-csp script

--- go-csp/prodcon.go
+++ go-csp/prodcon.go
@@ -12,20 +12,20 @@ var msgs = make(chan int, 10)
 
 func produce() {
 	for i := 0; i < 10; i++ {
-		msgs <- i
+		msgs <- i // Send the item to the consumer1
 		time.Sleep(time.Second)
 	}
-	done <- true
+	done <- true // Let main know that producer is done
 	fmt.Println("Producer is done.\n")
 }
 
 func consume() {
 	for {
-		msg := <-msgs
+		msg := <-msgs // read the item from the channel
 		fmt.Println(msg)
 		time.Sleep(time.Second * 2)
 		if 9 == msg {
-			done <- true
+			done <- true // Let main know that consumer is done
 			fmt.Println("Consumer is done.\n")
 		}
 	}
